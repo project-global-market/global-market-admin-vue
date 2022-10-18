2b17576887f2e86b022d66796d9a56771317c5c9
Ihor Tolkachov <ig0rehatrader@gmail.com>
2022-10-19 00:12:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const num = '5';
+console.log(num);
+
+export default defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      //
+    };
+  },
+});
+</script>
